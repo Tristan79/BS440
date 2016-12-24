bfec77b3c52cbf9faa27a9cc092346d11ca6e696
Tristan Crispijn <Tristan@MacBook-Pro.local>
2016-12-24 02:42:12 +0100

    .

--- BS440domoticz.py
+++ BS440domoticz.py
@@ -73,16 +73,23 @@ def UpdateDomoticz(config, weightdata, bodydata, persondata):
                   scaleuser, tbwid, bodydata[0]['tbw']))
         callurl('http://%s/json.htm?type=command&param=udevice&idx=%s&nvalue=0&svalue=%s' % (
                domoticzurl, tbwid, bodydata[0]['tbw']))
+        
+        try:
+            for i in persondata:
+                if i['person'] == bodydata[0]['person']:
+                    size = i['size'] / 100.0
 
-        for i in persondata:
-            if i['person'] == bodydata[0]['person']:
-                size = i['size'] / 100.0
+            print str(size)
+            bmi = weightdata[0]['weight'] / (size * size)
 
-        bmi = weightdata[0]['weight'] / (size * size)
-        log.info('Updating Domoticz for user %s at index %s with BMI %s' % (
-                  scaleuser, bmiid, bodydata[0]['bmi']))
-        callurl('http://%s/json.htm?type=command&param=udevice&idx=%s&nvalue=0&svalue=%s' % (
-               domoticzurl, bmiid, bmi))
+            print str(bmi)
+
+            log.info('Updating Domoticz for user %s at index %s with BMI %s' % (
+                      scaleuser, bmiid, bodydata[0]['bmi']))
+            callurl('http://%s/json.htm?type=command&param=udevice&idx=%s&nvalue=0&svalue=%s' % (
+                   domoticzurl, bmiid, bmi))
+        except Exception, e:
+            print str(e)
 
         log.info('Domoticz succesfully updated')
     except:
