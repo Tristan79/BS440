a87952e076880b2a0f2da72f751f85a84aa8a1ef
Tristan Crispijn <Tristan@MacBook-Pro.local>
2016-12-24 10:18:21 +0100

    .

--- BS440domoticz.py
+++ BS440domoticz.py
@@ -127,6 +127,7 @@ def UpdateDomoticz(config, weightdata, bodydata, persondata):
     def rename_realid(id,newname):
         query = True
         (idx, name) = exists_realid(id)
+        print "HERE"
         if name == "Unknown":
             rename_sensors(idx,newname)
             query = True
